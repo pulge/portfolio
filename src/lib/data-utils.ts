02085d492ad4565b0434501f27708fb1890c6cdc
Mark Jason Fulguerinas <markjasonrfulguerinas@iskolarngbayan.pup.edu.ph>
2025-12-02 01:07:24 +0800

    Update data-utils.ts

--- src/lib/data-utils.ts
+++ src/lib/data-utils.ts
@@ -24,8 +24,8 @@ export async function getAllPostsAndSubposts(): Promise<
 export async function getAllProjects(): Promise<CollectionEntry<'projects'>[]> {
   const projects = await getCollection('projects')
   return projects.sort((a, b) => {
-    const dateA = a.data.date?.getTime() || 0
-    const dateB = b.data.date?.getTime() || 0
+    const dateA = a.data.startDate?.getTime() || 0
+    const dateB = b.data.startDate?.getTime() || 0
     return dateB - dateA
   })
 }
