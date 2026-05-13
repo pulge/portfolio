b3f35b209450c2dc7eecaea0d1d9a8182ca23b70
mark <fulguerinasmarkjason0311@gmail.com>
2026-05-13 13:31:14 +0800

    Revert "Merge pull request #1 from pulge/cloudflare/workers-autoconfig"
    This reverts commit 738884597fd1dc61a98202f51bf2a894b70e31e8, reversing
    changes made to 98574cf298d0e960933b9732dfa7c58a8621a04b.

--- astro.config.ts
+++ astro.config.ts
@@ -18,12 +18,9 @@ import { pluginLineNumbers } from '@expressive-code/plugin-line-numbers'
 
 import tailwindcss from '@tailwindcss/vite'
 
-import cloudflare from "@astrojs/cloudflare";
-
 export default defineConfig({
   site: 'https://pulge.vercel.app',
   srcDir: './src',
-
   integrations: [
     expressiveCode({
       themes: ['github-light', 'github-dark'],
@@ -73,20 +70,16 @@ export default defineConfig({
     sitemap(),
     icon(),
   ],
-
   vite: {
     plugins: [tailwindcss()],
   },
-
   server: {
     port: 1234,
     host: true,
   },
-
   devToolbar: {
     enabled: false,
   },
-
   markdown: {
     syntaxHighlight: false,
     rehypePlugins: [
@@ -111,6 +104,4 @@ export default defineConfig({
     ],
     remarkPlugins: [remarkMath, remarkEmoji],
   },
-
-  adapter: cloudflare()
 })
